1f1daaf2c3e590da55433dc3f6479b3fe7de4638
critterday <tjons26@wooster.edu>
2025-02-12 13:58:00 -0500

    fixing my git

--- lab4.py
+++ lab4.py
@@ -1,7 +1,7 @@
-class Stack:
-    def createstack(name):
-        name = list()
+# class Stack:
+#     def createstack(name):
+  #      name = list()
 
-    def push(stack, item):
-        stack.append(item)
-    def pop(stack):
+   # def push(stack, item):
+ #      stack.append(item)
+ #  def pop(stack):
